ce0565f9a998eb9955a64348cec8363fd6f71821
Maxim Yakovlev <it.trainee@yandex.ru>
2024-11-28 23:52:53 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+'CREATE TABLE someName (name varchar(80), location point);'
+'SELECT * INTO someTableName FROM anotherTable;'
+'CREATE TABLE tableName AS SELECT * FROM someTable;'
+'CREATE TEMPORARY TABLE someName (code char(5), title varchar(40), did integer, date_prod date, kind varchar(10), len interval hour to minute, CONSTRAINT code_title PRIMARY KEY(code,title));'
